5b8ebccb8e78e99b4f9ab039ce2614c721b43167
sange <sange@hp>
2025-08-29 21:30:59 +0530

    Commit

---
+++ C2TC_JAVACORECONCEPTS/src/com/tnsif/dayfifteen/generics/MinMax.java
@@ -0,0 +1,10 @@
+//Program to demonstrate generic interface
+package com.tnsif.dayfifteen.generics;
+
+//Generic Interface
+public interface MinMax<T extends Comparable<T>> {
+
+	T min();
+
+	T max();
+}
